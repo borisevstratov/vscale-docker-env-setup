becce638943f5036b75f9ee97dcdb3b4020ac698
Boris Evstratov <borisevstratov@gmail.com>
2022-09-01 01:23:32 +0300

    README.md

---
+++ README.md
@@ -0,0 +1,33 @@
+
+# vscale-docker-env-setup
+
+Bash script that installs PostgreSQL, GitHub CLI and creates 4Gb swap partition
+
+## Installation
+
+Run script with `wget`
+
+```bash
+  wget https://raw.githubusercontent.com/borisevstratov/vscale-docker-env-setup/main/setup.sh && chmod +x bitrix-env.sh && ./bitrix-env.sh
+```
+
+## Post-installation
+
+Authenticate GitHub CLI with
+
+```bash
+echo -n "$GITHUB_AUTH_TOKEN" | gh auth login --with-token
+```
+
+Create PostgreSQL user & database with
+
+```bash
+sudo -u postgres psql
+```
+
+```sql
+CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_USER';
+ALTER ROLE $DB_USER CREATEDB;
+CREATE DATABASE $DB_NAME;
+
+```
--- setup.sh
+++ setup.sh
@@ -1,39 +1,8 @@
 #! /bin/sh
 set -x
 
-# Read args
-while getopts gh:pg:sw: flag; do
-    case "${flag}" in
-    gh) GITHUB_TOKEN=${OPTARG} ;;
-    pg) POSTGRES_NAME=${OPTARG} ;;
-    sw) SWAP_SIZE=${OPTARG} ;;
-    esac
-done
-
-# Write credentials
-echo "$GITHUB_TOKEN" >.githubtoken
-unset GITHUB_TOKEN
-
 # Update dependencies
 apt update -y
-
-# Install GitHub CLI
-apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-apt-add-repository https://cli.github.com/packages
-apt update
-apt install gh
-
-# Login to Github CLI
-gh auth login --with-token <.githubtoken
-rm .githubtoken
-
-# Install PostgreSQL
-apt update -y
-apt install postgresql postgresql-contrib -y
-
-sudo -u postgres psql -c "CREATE ROLE $POSTGRES_NAME WITH LOGIN PASSWORD '$POSTGRES_NAME'"
-sudo -u postgres psql -c "ALTER ROLE $POSTGRES_NAME CREATEDB"
-sudo -u postgres psql -c "CREATE DATABASE $POSTGRES_NAME"
 
 # Setup Swap
 fallocate -l $SWAP_SIZE /swapfile
@@ -44,3 +13,12 @@ sudo swapon --show
 free -h
 sudo cp /etc/fstab /etc/fstab.bak
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# Install GitHub CLI
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+apt-add-repository https://cli.github.com/packages
+apt update
+apt install gh
+
+# Install PostgreSQL
+apt install postgresql postgresql-contrib -y
