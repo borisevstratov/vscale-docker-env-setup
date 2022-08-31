ccda3d2f00c23eb0ecd55bfa95b67650c3650265
Boris Evstratov <borisevstratov@gmail.com>
2022-09-01 01:30:45 +0300

    Script comments

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ set -x
 
 # Update dependencies
 apt update -y
+echo "*** Updated dependencies ***"
 
 # Setup Swap
 fallocate -l $SWAP_SIZE /swapfile
@@ -13,11 +14,14 @@ sudo swapon --show
 free -h
 sudo cp /etc/fstab /etc/fstab.bak
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+echo "*** Allocated swap file ***"
 
 # Install GitHub CLI
 apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 apt-add-repository https://cli.github.com/packages
 apt install gh -y
+echo "*** Installed GitHub CLI ***"
 
 # Install PostgreSQL
 apt install postgresql postgresql-contrib -y
+echo "*** Installed PostgreSQL ***"
