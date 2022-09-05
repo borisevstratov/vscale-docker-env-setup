ceefbafa5b002c3fc895a0925b3cf50c93ac4826
Boris Evstratov <48457361+borisevstratov@users.noreply.github.com>
2022-09-05 11:40:00 +0300

    Updated GitHub CLI installation section

--- setup.sh
+++ setup.sh
@@ -30,9 +30,11 @@ echo "\nAllocated swap file"
 echo "----------------------------------------------------------------"
 
 # Install GitHub CLI
-apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-apt-add-repository https://cli.github.com/packages
-apt install gh -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null 
+sudo apt update 
+sudo apt install gh -y
 echo "----------------------------------------------------------------"
 echo "\nInstalled GitHub CLI"
 echo "----------------------------------------------------------------"
